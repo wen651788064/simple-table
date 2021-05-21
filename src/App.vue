a9a53374528cdd9a4de42f065cf039daceffc73f
done <1213686190@qq.com>
2021-05-21 16:48:48 +0800

    chore: Build the project and do some initial configuration

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-
+  <div>
+  </div>
 </template>
 
 <script>
@@ -7,17 +8,14 @@
 export default {
   name: 'App',
   components: {
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
